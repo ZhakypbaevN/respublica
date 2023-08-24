2f02fa752b6b0e205ecf17754633fe0136ff4452
korotun1910 <korotun1910@gmail.com>
2023-08-24 23:10:54 +0600

    add news page, add icon for page about

--- src/views/uiLanding/News.vue
+++ src/views/uiLanding/News.vue
@@ -2,14 +2,34 @@
   <div class="wrapper-main">
     <div>
       <Header />
-      <section class=" landing-block">
-        <div class="wrapper landing-wrapper">
-          <h2 class="landing-title">Новости</h2>
-          <NewsList />
+      <div class="content">
+        <div class="wrapper-menu">
+          <div class="left-menu">
+            <div class="left-menu-about">
+              <SvgIcon name="comment-alt-lines" :viewboxWidth="28" :viewboxHeight="28" />
+              <span>О партии</span>
 
-
+            </div>
+            <div class="left-menu-list">
+              <ul>
+                <li>Новости</li>
+                <li>Пресса о нас</li>
+                <li>Видеогалерея</li>
+                <li>Фотогалерея</li>
+                <li>Анонсы, объявления</li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </section>
+        <section class="landing-block">
+          <div class="wrapper landing-wrapper">
+            <h2 class="landing-title">Новости</h2>
+            <NewsList />
+
+
+          </div>
+        </section>
+      </div>
     </div>
 
     <Footer />
@@ -28,11 +48,70 @@ import NewsList from '../../components/uiLanding/news/NewsList.vue';
 
 }
 
+.landing-block {
+  margin-left: 40px;
+}
+
+.content {
+  display: flex;
+
+}
+.wrapper-menu {
+  width: 325px;
+  background: #042051;
+}
+.left-menu {
+  top: 20px;
+
+  
+  height: 300px;
+  position: sticky;
+  
+
+  &-about {
+    width: 285px;
+    height: 56px;
+    border-radius: 10px;
+    background: #0B2960;
+    margin-left: 20px;
+    margin-top: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & span {
+      color: #FFF;
+      font-size: 18px;
+      font-style: normal;
+      font-weight: 700;
+    }
+
+    & svg {
+      width: 44px;
+      height: 44px;
+      fill: var(--accent-color);
+    }
+  }
+
+  &-list {
+    margin-top: 27px;
+    margin-left: 47px;
+
+    & ul {
+      & li {
+        color: #FFF;
+        font-size: 18px;
+        font-weight: 400;
+        margin-top: 20px;
+      }
+    }
+  }
+
+}
+
 .landing-title {
   font-size: 36px;
   font-style: normal;
   font-weight: 700;
   color: #042051;
-}
-
-</style>
+}</style>
