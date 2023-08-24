af728297caa6386139df3c248aaffd2b3f490471
korotun1910 <korotun1910@gmail.com>
2023-08-24 14:33:37 +0600

    add news

--- src/views/uiLanding/News.vue
+++ src/views/uiLanding/News.vue
@@ -2,37 +2,37 @@
   <div class="wrapper-main">
     <div>
       <Header />
+      <section class=" landing-block">
+        <div class="wrapper landing-wrapper">
+          <h2 class="landing-title">Новости</h2>
+          <NewsList />
 
-      <div class="wrapper">
-        <h2 class="landing-title">Новости</h2>
-      </div>
+
+        </div>
+      </section>
     </div>
 
-    <Footer /> 
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from '../../components/uiLanding/layouts/header.vue';
 import Footer from '../../components/uiLanding/layouts/footer.vue';
+import NewsList from '../../components/uiLanding/news/NewsList.vue';
 </script>
 
 <style scoped lang="scss">
-.intro {
-  padding-top: 34%;
-  background: url('/img/uiLanding/intro-preview.jpg') center no-repeat;
-  background-size: cover;
+.wrapper-main {
+  background-color: #F6F9FD;
 
-  position: relative;
-
-  &-btns {
-    display: flex;
-    grid-gap: 30px;
-
-    position: absolute;
-    left: 50%;
-    bottom: 50px;
-    transform: translateX(-50%);
-  }
 }
+
+.landing-title {
+  font-size: 36px;
+  font-style: normal;
+  font-weight: 700;
+  color: #042051;
+}
+
 </style>
