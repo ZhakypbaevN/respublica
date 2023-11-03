28c1ab2ef59bac3b0fa8664041b8ffb70cf71def
korotun1910 <korotun1910@gmail.com>
2023-11-03 13:09:10 +0600

    add marketplace

---
+++ src/views/uiLanding/Marketplace.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="wrapper-main">
+      <div class="content">
+        <Header />
+        
+        <section class="landing-block">
+          <div class="wrapper landing-wrapper">
+            <h2 class="landing-title">Функционал находиться в разработке.</h2>
+        </div>
+      </section>
+    </div>
+
+    <Footer />
+  </div>
+</template>
