1e73cecbcf7a7c8a3c7105f8c2c1997d0c0eb02f
Nurzhigit Zhakypbaev <zhakypbaev.n4489@gmail.com>
2024-02-07 11:17:01 +0600

    bug/fix kz edit text жаңа мүмкіндіктер -> жаңа қадамдар

--- src/actions/uiClient/party-data.ts
+++ src/actions/uiClient/party-data.ts
@@ -1,4 +1,4 @@
 import api from '@/modules/api'
 
-export const postJoinParty = (data: FormData) =>
+export const postJoinParty = (data: any) =>
   api.asyncPost('/api/v1/parties/memberships', data)
