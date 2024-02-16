10781fcb0d1a535ba042d5bd86443784ec773e56
Nurzhigit Zhakypbaev <zhakypbaev.n4489@gmail.com>
2024-02-16 14:59:30 +0600

    new edit by text and intro buttons edit style

--- src/helpers/convertDateTime.js
+++ src/helpers/convertDateTime.js
@@ -1,5 +1,34 @@
 import { defaultLocale } from '@/assets/lang/exports'
 
+function formatDateKazakh(inputDate) {
+  if (!(inputDate instanceof Date)) {
+    throw new Error("Пожалуйста, передайте объект Date в качестве аргумента.");
+  }
+
+  const monthNamesKazakh = [
+    "қаңтар", "ақпан", "наурыз",
+    "сәуір", "мамыр", "маусым",
+    "шілде", "тамыз", "қыркүйек",
+    "қазан", "қараша", "желтоқсан"
+  ];
+
+  const yearEndingsKazakh = ["ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж.", "ж."];
+
+  const day = inputDate.getDate();
+  const month = inputDate.getMonth();
+  const year = inputDate.getFullYear();
+
+  const formattedDate = `${day} ${monthNamesKazakh[month]} ${year} ${yearEndingsKazakh[year % 10]}`;
+
+  return formattedDate;
+}
+
+// Пример использования
+const currentDate = new Date();
+const formattedDateStringKazakh = formatDateKazakh(currentDate);
+console.log(formattedDateStringKazakh);
+
+
 function convertDateTime(datetimeStr) {
   const datetime = new Date(datetimeStr);
 
@@ -9,14 +38,8 @@ function convertDateTime(datetimeStr) {
     day: 'numeric',
   };
 
-  const optionsKazakh = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  };
-
   const russianDateTime = datetime.toLocaleString('ru-RU', optionsRussian);
-  const kazakhDateTime = datetime.toLocaleString('kk-KZ', optionsKazakh);
+  const kazakhDateTime = formatDateKazakh(datetime);
 
   return defaultLocale == 'ru' ? russianDateTime : kazakhDateTime;
 }
